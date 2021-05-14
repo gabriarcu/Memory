afee9b2533fc9024a9ebfad351e460d1051ab3b0
gabriarcu <38346968+gabriarcu@users.noreply.github.com>
2021-05-14 21:56:17 +0200

    Aggiungere i file di progetto.

---
+++ Memory/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿
+namespace Memory
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btn_esci = new System.Windows.Forms.Button();
+            this.btn_punteggi = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btn_newGame = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.opzioniToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.livelloToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.facileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.medioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.difficileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.SteelBlue;
+            this.panel1.Controls.Add(this.btn_esci);
+            this.panel1.Controls.Add(this.btn_punteggi);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.btn_newGame);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(134, 557);
+            this.panel1.TabIndex = 0;
+            // 
+            // btn_esci
+            // 
+            this.btn_esci.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btn_esci.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_esci.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_esci.ForeColor = System.Drawing.Color.SteelBlue;
+            this.btn_esci.Location = new System.Drawing.Point(3, 220);
+            this.btn_esci.Name = "btn_esci";
+            this.btn_esci.Size = new System.Drawing.Size(127, 40);
+            this.btn_esci.TabIndex = 3;
+            this.btn_esci.Text = "Esci";
+            this.btn_esci.UseVisualStyleBackColor = false;
+            this.btn_esci.Click += new System.EventHandler(this.btn_esci_Click);
+            // 
+            // btn_punteggi
+            // 
+            this.btn_punteggi.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btn_punteggi.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_punteggi.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_punteggi.ForeColor = System.Drawing.Color.SteelBlue;
+            this.btn_punteggi.Location = new System.Drawing.Point(3, 165);
+            this.btn_punteggi.Name = "btn_punteggi";
+            this.btn_punteggi.Size = new System.Drawing.Size(127, 40);
+            this.btn_punteggi.TabIndex = 2;
+            this.btn_punteggi.Text = "Punteggi";
+            this.btn_punteggi.UseVisualStyleBackColor = false;
+            this.btn_punteggi.Click += new System.EventHandler(this.btn_punteggi_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pictureBox1.Image = global::Memory.Properties.Resources.logo;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(134, 104);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btn_newGame
+            // 
+            this.btn_newGame.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btn_newGame.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_newGame.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_newGame.ForeColor = System.Drawing.Color.SteelBlue;
+            this.btn_newGame.Location = new System.Drawing.Point(3, 110);
+            this.btn_newGame.Name = "btn_newGame";
+            this.btn_newGame.Size = new System.Drawing.Size(127, 40);
+            this.btn_newGame.TabIndex = 0;
+            this.btn_newGame.Text = "New Game";
+            this.btn_newGame.UseVisualStyleBackColor = false;
+            this.btn_newGame.Click += new System.EventHandler(this.btn_newGame_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.panel2.Controls.Add(this.tabControl1);
+            this.panel2.Controls.Add(this.menuStrip1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(134, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(980, 557);
+            this.panel2.TabIndex = 1;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 24);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(980, 533);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(972, 507);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(972, 507);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.opzioniToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(980, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // opzioniToolStripMenuItem
+            // 
+            this.opzioniToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.livelloToolStripMenuItem});
+            this.opzioniToolStripMenuItem.Name = "opzioniToolStripMenuItem";
+            this.opzioniToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.opzioniToolStripMenuItem.Text = "Opzioni";
+            // 
+            // livelloToolStripMenuItem
+            // 
+            this.livelloToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.facileToolStripMenuItem,
+            this.medioToolStripMenuItem,
+            this.difficileToolStripMenuItem});
+            this.livelloToolStripMenuItem.Name = "livelloToolStripMenuItem";
+            this.livelloToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.livelloToolStripMenuItem.Text = "Livello";
+            // 
+            // facileToolStripMenuItem
+            // 
+            this.facileToolStripMenuItem.Name = "facileToolStripMenuItem";
+            this.facileToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.facileToolStripMenuItem.Text = "Facile";
+            this.facileToolStripMenuItem.Click += new System.EventHandler(this.facileToolStripMenuItem_Click);
+            // 
+            // medioToolStripMenuItem
+            // 
+            this.medioToolStripMenuItem.Name = "medioToolStripMenuItem";
+            this.medioToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.medioToolStripMenuItem.Text = "Medio";
+            this.medioToolStripMenuItem.Click += new System.EventHandler(this.medioToolStripMenuItem_Click);
+            // 
+            // difficileToolStripMenuItem
+            // 
+            this.difficileToolStripMenuItem.Name = "difficileToolStripMenuItem";
+            this.difficileToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.difficileToolStripMenuItem.Text = "Difficile";
+            this.difficileToolStripMenuItem.Click += new System.EventHandler(this.difficileToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1114, 557);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Memory Game";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btn_newGame;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btn_esci;
+        private System.Windows.Forms.Button btn_punteggi;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem opzioniToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem livelloToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem facileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem medioToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem difficileToolStripMenuItem;
+    }
+}
+
---
+++ Memory/Form1.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Memory
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            tabControl1.Appearance = TabAppearance.FlatButtons;
+            tabControl1.ItemSize = new Size(0, 1);
+            tabControl1.SizeMode = TabSizeMode.Fixed;
+            facileToolStripMenuItem.Checked = true;
+            facileToolStripMenuItem.Checked = true;
+            medioToolStripMenuItem.Checked = false;
+            difficileToolStripMenuItem.Checked = false;
+            tabControl1.TabPages[0].Controls.Clear();
+            int x = 1;
+
+            int cy = 18;
+            int cy2 = 18;
+            int cy3 = 18;
+            while (x < 10)
+            {
+                PictureBox t1 = new PictureBox();
+                t1.Name = $"carta_{x + 1}";
+                if (x < 4)
+                {
+                    t1.Location = new Point((cy), 15);
+                    cy = cy + 131;
+                }
+
+                if (x > 3 && x < 7)
+                {
+                    t1.Location = new Point(cy2, 157);
+                    cy2 = cy2 + 131;
+                }
+
+                if (x > 6 && x < 10)
+                {
+                    t1.Location = new Point((cy3), 295);
+                    cy3 = cy3 + 131;
+                }
+
+                t1.ImageLocation = (@"..\..\Resources\dorso.png");
+
+                t1.BackgroundImageLayout = ImageLayout.Stretch;
+                t1.SizeMode = PictureBoxSizeMode.StretchImage;
+                t1.AutoSize = false;
+                t1.Width = 114;
+                t1.Height = 122;
+
+                t1.Visible = true;
+                tabControl1.TabPages[0].Controls.Add(t1);
+
+                GroupBox t2 = new GroupBox();
+                t2.Location=new Point((18), 420);
+                t2.Width = 950;
+                t2.Text = "Dati";
+                tabControl1.TabPages[0].Controls.Add(t2);
+
+                x = x + 1;
+            }
+        }
+
+        private void facileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            facileToolStripMenuItem.Checked = true;
+            medioToolStripMenuItem.Checked = false;
+            difficileToolStripMenuItem.Checked = false;
+            tabControl1.TabPages[0].Controls.Clear();
+            int x = 1;
+           
+            int cy = 18;
+            int cy2 = 18;
+            int cy3 = 18;
+            while (x<10)
+            {
+                PictureBox t1 = new PictureBox();
+                t1.Name = $"carta_{x + 1}";
+                if(x<4)
+                {
+                    t1.Location = new Point((cy),15);
+                    cy = cy + 131;
+                }
+                
+                if (x > 3 && x < 7)
+                {
+                    t1.Location = new Point(cy2, 157);
+                    cy2 = cy2 + 131;
+                }
+               
+                if (x > 6 && x < 10)
+                {
+                    t1.Location = new Point((cy3), 295);
+                    cy3 = cy3 + 131;
+                }
+
+                t1.ImageLocation = (@"..\..\Resources\dorso.png");
+            
+                t1.BackgroundImageLayout = ImageLayout.Stretch;
+                t1.SizeMode = PictureBoxSizeMode.StretchImage;
+                t1.AutoSize = false;
+                t1.Width = 114;
+                t1.Height = 122;
+                
+                t1.Visible = true;
+                tabControl1.TabPages[0].Controls.Add(t1);
+
+                GroupBox t2 = new GroupBox();
+                t2.Location = new Point((18), 420);
+                t2.Width = 950;
+                t2.Text = "Dati";
+                tabControl1.TabPages[0].Controls.Add(t2);
+
+
+
+                x = x + 1;
+            }
+        }
+        private void medioToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            tabControl1.TabPages[0].Controls.Clear();
+            medioToolStripMenuItem.Checked = true;
+            facileToolStripMenuItem.Checked = false;
+            difficileToolStripMenuItem.Checked = false;
+            int x = 1;
+
+            int cy = 18;
+            int cy2 = 18;
+            int cy3 = 18;
+            while (x < 16)
+            {
+                PictureBox t1 = new PictureBox();
+                t1.Name = $"carta_{x + 1}";
+                if (x < 6)
+                {
+                    t1.Location = new Point((cy), 15);
+                    cy = cy + 131;
+                }
+
+                if (x > 5 && x < 11)
+                {
+                    t1.Location = new Point(cy2, 157);
+                    cy2 = cy2 + 131;
+                }
+
+                if (x > 10 && x < 16)
+                {
+                    t1.Location = new Point((cy3), 295);
+                    cy3 = cy3 + 131;
+                }
+
+                t1.ImageLocation = (@"..\..\Resources\dorso.png");
+
+                t1.BackgroundImageLayout = ImageLayout.Stretch;
+                t1.SizeMode = PictureBoxSizeMode.StretchImage;
+                t1.AutoSize = false;
+                t1.Width = 114;
+                t1.Height = 122;
+
+                t1.Visible = true;
+                tabControl1.TabPages[0].Controls.Add(t1);
+
+                GroupBox t2 = new GroupBox();
+                t2.Location = new Point((18), 420);
+                t2.Width = 950;
+                t2.Text = "Dati";
+                tabControl1.TabPages[0].Controls.Add(t2);
+
+
+                x = x + 1;
+            }
+        }
+
+        private void difficileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            tabControl1.TabPages[0].Controls.Clear();
+            medioToolStripMenuItem.Checked = false;
+            facileToolStripMenuItem.Checked = false;
+            difficileToolStripMenuItem.Checked = true;
+            int x = 1;
+
+            int cy = 18;
+            int cy2 = 18;
+            int cy3 = 18;
+            while (x < 22)
+            {
+                PictureBox t1 = new PictureBox();
+                t1.Name = $"carta_{x + 1}";
+                if (x < 8)
+                {
+                    t1.Location = new Point((cy), 15);
+                    cy = cy + 131;
+                }
+
+                if (x > 7 && x < 15)
+                {
+                    t1.Location = new Point(cy2, 157);
+                    cy2 = cy2 + 131;
+                }
+
+                if (x > 14 && x < 22)
+                {
+                    t1.Location = new Point((cy3), 295);
+                    cy3 = cy3 + 131;
+                }
+
+                t1.ImageLocation = (@"..\..\Resources\dorso.png");
+
+                t1.BackgroundImageLayout = ImageLayout.Stretch;
+                t1.SizeMode = PictureBoxSizeMode.StretchImage;
+                t1.AutoSize = false;
+                t1.Width = 114;
+                t1.Height = 122;
+
+                t1.Visible = true;
+                tabControl1.TabPages[0].Controls.Add(t1);
+
+                GroupBox t2 = new GroupBox();
+                t2.Location = new Point((18), 420);
+                t2.Width = 950;
+                t2.Text = "Dati";
+                tabControl1.TabPages[0].Controls.Add(t2);
+
+
+                x = x + 1;
+            }
+        }
+
+        private void btn_newGame_Click(object sender, EventArgs e)
+        {
+            tabControl1.SelectTab(0);
+        }
+
+        private void btn_punteggi_Click(object sender, EventArgs e)
+        {
+            tabControl1.SelectTab(1);
+        }
+
+        private void btn_esci_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+       
+    }
+}
